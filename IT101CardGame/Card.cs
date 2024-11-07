4a60a9673072477dd52d9079441a6fe050dbca58
Jesse Harlan <jesse.harlan@centralia.edu>
2024-11-07 10:14:00 -0800

    Add project files.

---
+++ IT101CardGame/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT101CardGame
+{
+    public class Card
+    {
+        public int Rank { get; private set; }
+        public string Suit { get; private set; }
+
+        public Card(int rank, string suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        public override string? ToString()
+        {
+            return $"{ RankToString() } of { Suit }";
+        }
+
+        private string RankToString()
+        {
+            string value = "";
+
+            switch (Rank)
+            {
+                case 1:
+                    value = "Ace";
+                    break;
+                case 11:
+                    value = "Jack";
+                    break;
+                case 12:
+                    value = "Queen";
+                    break;
+                case 13:
+                    value = "King";
+                    break;
+                default:
+                    value = Rank.ToString();
+                    break;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ IT101CardGame/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace IT101CardGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card[] deck = new Card[52];
+
+            for (int s = 0; s < 4; s++)
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    string suit = "";
+
+                    switch (s)
+                    {
+                        case 0:
+                            suit = "Hearts";
+                            break;
+                        case 1:
+                            suit = "Diamonds";
+                            break;
+                        case 2:
+                            suit = "Clubs";
+                            break;
+                        case 3:
+                            suit = "Spades";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    Card myCard = new Card(i, suit);
+
+                    Console.WriteLine(myCard.ToString());
+                }
+            }
+        }
+    }
+}
