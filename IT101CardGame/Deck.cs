23527dc4d7434b76267a52d3c650846268c96dea
Jesse Harlan <jesse.harlan@centralia.edu>
2024-11-12 10:06:10 -0800

    Added deck class

---
+++ IT101CardGame/Deck.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT101CardGame
+{
+    public class Deck
+    {
+        private List<Card> Cards;
+
+        public Deck(bool init = true)
+        {
+            Cards = new List<Card>();
+            // create a deck of cards
+            if (init)
+            {
+                ResetDeck();
+            }
+        }
+
+        public void ResetDeck()
+        {
+            // empty the deck
+            Cards.Clear();
+
+            // create a fresh deck
+            for (int s = 0; s < 4; s++)
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    string suit = "";
+
+                    switch (s)
+                    {
+                        case 0:
+                            suit = "Hearts";
+                            break;
+                        case 1:
+                            suit = "Diamonds";
+                            break;
+                        case 2:
+                            suit = "Clubs";
+                            break;
+                        case 3:
+                            suit = "Spades";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    Cards.Add(new Card(i, suit));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rand = new Random();
+
+            Cards = Cards.OrderBy(x => rand.Next()).ToList();
+        }
+
+        public Card Draw()
+        {
+            // get the first element of cards
+            Card card = Cards[0];
+
+            // delete the first element from cards
+            Cards.RemoveAt(0);
+
+            // return the saved element
+            return card;
+        }
+
+        public void AddCard(Card card)
+        {
+            Cards.Add(card);
+        }
+    }
+}
--- IT101CardGame/Program.cs
+++ IT101CardGame/Program.cs
@@ -4,37 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Card[] deck = new Card[52];
+            Deck myDeck = new Deck();
+            Deck hand = new Deck(false);
+            myDeck.Shuffle();
 
-            for (int s = 0; s < 4; s++)
+            for (int i = 0; i < 2; i++)
             {
-                for (int i = 1; i <= 13; i++)
-                {
-                    string suit = "";
-
-                    switch (s)
-                    {
-                        case 0:
-                            suit = "Hearts";
-                            break;
-                        case 1:
-                            suit = "Diamonds";
-                            break;
-                        case 2:
-                            suit = "Clubs";
-                            break;
-                        case 3:
-                            suit = "Spades";
-                            break;
-                        default:
-                            break;
-                    }
-
-                    Card myCard = new Card(i, suit);
-
-                    Console.WriteLine(myCard.ToString());
-                }
+                hand.AddCard(myDeck.Draw());
             }
+
+            Console.WriteLine();
         }
     }
 }
